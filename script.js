986ba18c977ac226b1f6ec70353682ff7333c59c
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-07-25 22:29:40 +0200

    selected items in js

---
+++ script.js
@@ -0,0 +1,7 @@
+const result = document.querySelector(".result");
+const playerChoice = document.querySelector(".player-choice");
+const computerChoice = document.querySelector(".computer-choice");
+const option = document.querySelectorAll(".option");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
